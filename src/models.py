3ed56f9142d0006ce0b7bb47a351f1dde734330c
Roberto Agredo <memoriaderam@gmail.com>
2025-03-07 20:10:20 -0400

    Update models.py
    last

--- src/models.py
+++ src/models.py
@@ -1,30 +1,97 @@
-import os
-import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import create_engine, String, ForeignKey
-from eralchemy2 import render_er
 
-Base = declarative_base()
+db = SQLAlchemy()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User (db.Model):
+
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    email: Mapped[str] = mapped_column(nullable=False,unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    date: Mapped[str] = mapped_column(nullable=False)
+    personaje = db.relationship('Personaje')
+    planet = db.relationship('Planet')
+    specie = db.relationship('Specie')
+    vehicle = db.relationship('Vehicle')
 
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def serialize_user(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'password': self.password,
+            'date': self.date,
+          
+        }
+ 
+  
+class Personaje (db.Model):
+    __tablename__ = 'personaje'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    firstname_per: Mapped[str] = mapped_column(nullable=False,unique=True)
+    descrip_per: Mapped[str] = mapped_column(nullable=False)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+    def serialize_personaje(self):
+        return {
+            'id': self.id,
+            'firstname_per': self.firstname_per,
+            'descrip_per': self.descrip_per,
+            'user_id': self.meta_keywords,
+            
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    firstname_pla: Mapped[str] = mapped_column(nullable=False,unique=True)
+    descrip_pla: Mapped[str] = mapped_column(nullable=False)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+    def serialize_planet(self):
+         return {
+            'id': self.id,
+            'firstname_pla': self.firstname_per,
+            'descrip_pla': self.descrip_per,
+            'user_id': self.meta_keywords,
+            
+        }
+
+class Specie(db.Model):
+    __tablename__ = 'specie'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    firstname_spe: Mapped[str] = mapped_column(nullable=False,unique=True)
+    descrip_spe: Mapped[str] = mapped_column(nullable=False)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+    def serialize_specie(self):
+         return {
+            'id': self.id,
+            'firstname_spe': self.firstname_per,
+            'descrip_spe': self.descrip_per,
+            'user_id': self.meta_keywords,
+            
+        }
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    firstname_vehi: Mapped[str] = mapped_column(nullable=False,unique=True)
+    descrip_vehi: Mapped[str] = mapped_column(nullable=False)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+    def serialize_vehicle(self):
+         return {
+            'id': self.id,
+            'firstname_vehi': self.firstname_per,
+            'descrip_vehi': self.descrip_per,
+            'user_id': self.meta_keywords,
+            
+        }
 
     def to_dict(self):
         return {}
